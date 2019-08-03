35eca44b26cd7d6e939b9d7e65b4550c8eb682d7
Tulio Torezan Silingardi Del Claro <tuliotorezan@gmail.com>
2019-08-03 12:58:12 -0300

    GPU config

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
@@ -18,22 +18,24 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13].values
+X = dataset.iloc[:, 3:13].values  #everything excluding rownumber, id & surename
+y = dataset.iloc[:, 13].values #last column is the results
 
 # Encoding categorical data
+# will need to encode columns 1, 2 since one of them is the nacionality and the other one is gender
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
+
+onehotencoder = OneHotEncoder(categorical_features = [1]) #creating dummy variables since there are 3 countries in the dataset (for genders which are only two, no need for that)
 X = onehotencoder.fit_transform(X).toarray()
-X = X[:, 1:]
+X = X[:, 1:] #removing one of the 3 dummy variables to avoid the dummy variable trap
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) #20% for test the remainder for training
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -41,36 +43,42 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Now let's make the ANN!
 
-# Importing the Keras libraries and packages
-import keras
+################################################# UPWARD JUST DATA PREPROCESSING, HERE IS THE ACTUAL ANN #######################################################################
+
+#importing Keras library and packs
+import  keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-# Initialising the ANN
+#initializing the artificial neural network
 classifier = Sequential()
 
-# Adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+#building the input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer='uniform', activation = 'relu'))
 
-# Adding the second hidden layer
-classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+#second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer='uniform', activation = 'relu'))
 
-# Adding the output layer
-classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+#output layer. If i had 3 or 4 outcomes instead of only 2 possibilities (here its stay or leave, but could be stay active, go inactive or delete account for exemple), i would change units = 1 by units = 3 and sigmoid by softmax (which is basically the same but for more variables)
+classifier.add(Dense(units = 1, kernel_initializer='uniform', activation = 'sigmoid'))
 
-# Compiling the ANN
+#compiling the ANN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+#adam is a good stocastic gradient decent algorithim
 
-# Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+#fitting the network to the training dataset
+classifier.fit(X_train, y_train, batch_size = 10, epochs=100)
 
-# Part 3 - Making the predictions and evaluating the model
+
+###############################################3 NOW JUST COMPARING TO THE TEST DATASET #######################################################################################
+
+
+#Predicting and evaluating it
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
+y_pred = (y_pred > 0.5) #transforming from values between 0 and 1 to 0 or 1 (my treshold is at 50%, could be lower for sensitive cases like danger of mechanical failure)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
--- Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/cnn.py
+++ Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/cnn.py
@@ -11,6 +11,9 @@
 
 # Part 1 - Building the CNN
 
+import tensorflow as tf
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
 # Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Convolution2D
@@ -22,21 +25,21 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Convolution2D(32, 3, input_shape = (64, 64, 3), activation = 'relu'))
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Adding a second convolutional layer
-classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(Convolution2D(32, 3, activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
@@ -63,7 +66,7 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         samples_per_epoch = 8000,
-                         nb_epoch = 25,
+                         steps_per_epoch = 8000,
+                         epochs = 50,
                          validation_data = test_set,
-                         nb_val_samples = 2000)
+                         validation_steps = 2000)
