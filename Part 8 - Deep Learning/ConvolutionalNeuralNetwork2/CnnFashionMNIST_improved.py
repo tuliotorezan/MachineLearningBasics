f20f59297fa9cdde130c46e319d8cb917c080aaf
Tulio Torezan Silingardi Del Claro <tuliotorezan@gmail.com>
2019-08-26 18:57:16 -0300

    Fashion MNIST model improvement

--- Part 8 - Deep Learning/ConvolutionalNeuralNetwork2/CnnFashionMNIST_improved.py
+++ Part 8 - Deep Learning/ConvolutionalNeuralNetwork2/CnnFashionMNIST_improved.py
@@ -1,2 +1,84 @@
-# -*- coding: utf-8 -*-
+# -*- code by: Tulio Torezan -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__) #just checking (working on 1.14)
+
+#importing dataset from tensorflow keras datasets
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+#keeping track of the classes to translate later
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+###just exemples for ilustrating how to use in case i didnt already know the size and number of images ###############
+#checking dataset specifications
+train_images.shape
+
+#confirming the amount of labels is the same as the amount of images
+len(train_labels)
+train_labels
+test_images.shape
+len(test_labels)
+
+
+#### preprocessing immages to range from 0-1 instead of 0-255
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+
+train_images = train_images.reshape(60000, 28, 28, 1)
+test_images = test_images.reshape(10000, 28, 28, 1)
+
+
+model = tf.keras.Sequential()
+
+model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu', input_shape=(28,28,1)))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.3))
+
+model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
+model.add(tf.keras.layers.Dropout(0.3))
+
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128,
+                             activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128,
+                             activation=tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(10,
+                             activation=tf.nn.softmax))
+
+
+
+#configuring model compiler
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy', 'sparse_categorical_crossentropy'])
+
+
+#feeding and training the model
+model.fit(train_images,
+          train_labels,
+          batch_size=128,
+          epochs=25,
+          validation_data=(test_images, test_labels),
+          verbose=1)
+
+#evaluating model performance on the test set (in overfit cases it may get almost perfect for the train and then almost random selection for the test)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
 
---
+++ README.md
@@ -0,0 +1,2 @@
+# MachineLearningBasics
+Just to store any codes I write while studying machine learning, from the basics up to wherever I get. I will be taking some courses (starting by "Machine Learning A-Z") and studying on my own as well, so there might be courses material but I might upload some of my own annotations as well.
